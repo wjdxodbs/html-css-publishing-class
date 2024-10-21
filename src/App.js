e4a36637048903e3218774ce4d41fd3614c98739
wjdxodbs <wjdxodbs52@naver.com>
2024-10-21 17:54:31 +0900

    css 자손선택자 vs 자식선택자, 부모요소 vs 자식요소

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Html5Core from "./components/Html5Core";
 import CssCore from "./components/CssCore";
 import CssFormatting from "./components/CssFormatting";
+import CssSelecter from "./components/CssSelecter";
 
 function App() {
-  return <CssFormatting />;
+  return <CssSelecter />;
 }
 
 export default App;
---
+++ src/components/CssSelecter.jsx
@@ -0,0 +1,19 @@
+import "./CssSelecter.css";
+
+const CssSelecter = () => {
+  return (
+    // <div className="parent">
+    //   <div className="child">
+    //     <div></div>
+    //   </div>
+    // </div>
+
+    <div className="box">
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default CssSelecter;
