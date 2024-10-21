272d1730b87c8b416db681a8b046a7503c409dfd
wjdxodbs <wjdxodbs52@naver.com>
2024-10-21 11:21:57 +0900

    html5 핵심 태그의 사용법

---
+++ src/components/FormatPractice.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const FormatPractice = () => {
+  return (
+    <>
+      {/* <h1>무의미한 한글 텍스트 생성기입니다.</h1>
+      <h2>무의미한 한글 텍스트 생성기입니다.</h2>
+      <h3>무의미한 한글 텍스트 생성기입니다.</h3>
+      <h4>무의미한 한글 텍스트 생성기입니다.</h4>
+      <h5>무의미한 한글 텍스트 생성기입니다.</h5>
+      <h6>무의미한 한글 텍스트 생성기입니다.</h6> */}
+      {/* <p>
+        <b>국가는 평생교육</b>을 진흥하여야 한다. <em>국회</em>의 정기회는
+        법률이 정하는 바에 의하여 매년 1회 집회되며, <u>국회의 임시회</u>는
+        대통령 또는 국회재적의원 <s>4분의 1</s> 이상의 <big>요구</big>에 의하여
+        집회된다.
+      </p>
+      <p>
+        <small>근로조건</small>의 기준은 인간의 존엄성을 보장하도록 법률로
+        정한다.
+        <br />
+        국회가 재적의원 과반수의 찬성으로 계엄의 해제를 요구한 때에는 대통령은
+        이를 해제하여야 한다.
+      </p>
+      <p>
+        국정감사 및 조사에 관한 절차 기타 필요<sup>한</sup> 사항은 법률
+        <sub>로</sub>
+        정한다. 국민의 <mark>자유와 권리</mark>는 헌법에 열거되지 아니한 이유로
+        경시되지 아니한다.
+        <hr />
+        모든 국민은 근로의 권리를 가진다. 국가는 사회적·경제적 방법으로 근로자의
+        고용의 증진과 적정임금의 보장에 노력하여야 하며, 법률이 정하는 바에
+        의하여 최저임금제를 시행하여야 한다.
+      </p> */}
+      {/* <ol>
+        <li>방콕 쇼핑 리스트</li>
+        <li>
+          방콕에서 반드시 들려야 할 베스트
+          <ul>
+            <li>씨암</li>
+            <li>카오산 로드</li>
+          </ul>
+        </li>
+        <li>방콕 맛집 리스트</li>
+        <li>방콕 관광 리스트</li>
+      </ol> */}
+      <a href="https://www.naver.com">
+        <img
+          src="https://i.namu.wiki/i/O_WfY01sM81pAk50fxe-CgHx-pVnipkiexM-biFWhdIBezk7_xJfDiu7P8hzraM5oNm0r92GCaXrfoZ14RNHVg.svg"
+          alt="Naver Site"
+        />
+      </a>
+      <a
+        href="https://www.google.com"
+        target="_blank"
+        title="구글사이트로 이동"
+      >
+        <img
+          src="https://i.namu.wiki/i/jCbiz6JK32E8JJXI5yRp0FeiWNZQ4pb11NmqdhGyxTgTSMZinZuydKhgiSSghasnaNIEuOj3QCORAl1kJGreXw.webp"
+          alt="Google Site"
+        />
+      </a>
+    </>
+  );
+};
+
+export default FormatPractice;
