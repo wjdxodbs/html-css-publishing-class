1529de985d83799badf3da25359f8647a76dc5e1
wjdxodbs <wjdxodbs52@naver.com>
2024-10-22 16:12:39 +0900

    상세 디자을 위한 css 박스모델

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import Html5Core from "./components/Html5Core";
 import CssCore from "./components/CssCore";
 import CssFormatting from "./components/CssFormatting";
 import CssSelecter from "./components/CssSelecter";
+import CssBoxModel from "./components/CssBoxModel";
 
 function App() {
-  return <CssSelecter />;
+  return <CssBoxModel />;
 }
 
 export default App;
---
+++ src/components/CssBoxModel.jsx
@@ -0,0 +1,35 @@
+import "./CssBoxModel.css";
+
+const CssBoxModel = () => {
+  return (
+    <>
+      {/* <div className="box"></div> */}
+
+      {/* <div className="box box1">box1</div>
+      <div className="box box2">box2</div>
+      <div className="box box3">box3</div>
+      <div className="box box4">box4</div>
+      <div className="box box5">box5</div>
+      <div className="box box6">box6</div>
+      <div className="box box7">box7</div>
+      <div className="box box8">box8</div> */}
+
+      {/* <div className="box">Box</div> */}
+      {/* <h1 className="heading">박스모델</h1> */}
+      {/* <div className="box">Box Content</div> */}
+
+      {/* <div className="box-shadow">
+        <div className="box-shadow1"></div>
+        <div className="box-shadow2"></div>
+        <div className="box-shadow3"></div>
+        <div className="box-shadow4"></div>
+        <div className="box-shadow5"></div>
+        <div className="box-shadow6"></div>
+      </div> */}
+
+      <div className="text-shadow">shadow</div>
+    </>
+  );
+};
+
+export default CssBoxModel;
